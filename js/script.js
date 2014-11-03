c31b0aa1fd2ab186d5af349dbe6981f9712a9450
Yoel Monzón <yoelfme@hotmail.com>
2014-11-02 23:56:57 -0600

    Refactorizando

--- js/script.js
+++ js/script.js
@@ -1,15 +1,32 @@
-var map = null;
-var fnStart = function(){
-  map = L.map('map').setView([14.582352, -91.623541], 11);
-  L.marker([14.582352,-91.623541]).addTo(map);
-  L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
-			maxZoom: 18,
-			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-				'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-				'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-			id: 'examples.map-i875mjb7'
-		}).addTo(map);
-}
+var exports = {};
+(function (window,L){
+    var map = null;
 
+    this.start = function (){
+        map = window.L.map('map').setView([14.582352, -91.623541], 11);
+        L.marker([14.582352,-91.623541]).addTo(map);
+        L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+            maxZoom: 18,
+            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+              '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+              'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+            id: 'examples.map-i875mjb7'
+          }).addTo(map);
+        //document.getElementById('contact').click();
+    };
 
-addEventListener('load',fnStart,false);
+    this.goTo = function (element, to, duration){
+      if (duration < 0) return;
+      var difference = to - element.scrollTop;
+      var perTick = difference / duration * 10;
+      setTimeout(function() {
+          element.scrollTop = element.scrollTop + perTick;
+          if (element.scrollTop === to) return;
+          exports.goTo(element, to, duration - 10);
+      }, 10);
+
+      return false;
+    };
+}).call(exports,window,L);
+
+addEventListener('load',exports.start,false);

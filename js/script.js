c131de25dfcfa79824993d4004743bc071006243
Yoel Monzón <yoelfme@hotmail.com>
2014-11-06 10:35:37 -0600

    Fix it litle

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var exports = {};
     var map = null;
 
     this.start = function (){
-        map = window.L.map('map').setView([14.582352, -91.623541], 11);
+        map = L.map('map').setView([14.582352, -91.623541], 11);
         L.marker([14.582352,-91.623541]).addTo(map);
         L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
             maxZoom: 18,
@@ -12,20 +12,15 @@ var exports = {};
               'Imagery © <a href="http://mapbox.com">Mapbox</a>',
             id: 'examples.map-i875mjb7'
           }).addTo(map);
-        //document.getElementById('contact').click();
     };
-
+    
     this.goTo = function (element, to, duration,last_difference){
       if (duration < 0) return;
-
       last_difference = typeof last_difference !== 'undefined' ? last_difference : -1;
-
       to = document.getElementById(to);
       var difference = to.offsetTop - element.scrollTop;
       var perTick = difference / duration * 10;
-      
       if (last_difference === difference ) return;
-
       setTimeout(function() {
           element.scrollTop = element.scrollTop + perTick;
           if (element.scrollTop === to.offsetTop) return;
